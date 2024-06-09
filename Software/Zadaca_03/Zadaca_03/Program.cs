edff95ffedc80248c1737a71350602324df8a182
Tin Muhar <tmuhar22@student.foi.hr>
2024-06-09 23:55:54 +0200

    Referencirana biblioteka za rad s bazom

---
+++ Software/Zadaca_03/Zadaca_03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DBLayer;
+
+namespace Zadaca_03
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            DB.SetConfiguration("PI2324_tmuhar22_DB", "PI2324_tmuhar22_User", "&l3RTM]d");
+            Application.Run(new Form1());
+        }
+    }
+}
