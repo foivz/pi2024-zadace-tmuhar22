08fea3ca4c837856dc850a48a56c161e55bc2a06
Tin Muhar <tmuhar22@student.foi.hr>
2024-06-11 01:40:28 +0200

    Dodana forma za recenziju

--- Software/Zadaca_03/Zadaca_03/FrmEvaluation.cs
+++ Software/Zadaca_03/Zadaca_03/FrmEvaluation.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Zadaca_03
 {
-    public partial class Form2 : Form
+    public partial class FrmEvaluation : Form
     {
-        public Form2()
+        public FrmEvaluation()
         {
             InitializeComponent();
         }
