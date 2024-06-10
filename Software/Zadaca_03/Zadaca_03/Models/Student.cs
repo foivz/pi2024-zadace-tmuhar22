33a1c0e6bc01c931421ab1525b4b4c47c756382e
Tin Muhar <tmuhar22@student.foi.hr>
2024-06-10 02:01:38 +0200

    Implementacija hijerarhije

---
+++ Software/Zadaca_03/Zadaca_03/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca_03.Models
+{
+    public class Student : Person
+    {
+        public int OrderNumber { get; set; }
+        public int Review { get; set; }
+        public string Komentar { get; set; }
+
+    }
+}
