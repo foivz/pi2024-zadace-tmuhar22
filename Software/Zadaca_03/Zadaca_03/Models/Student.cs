c80587791470c8a1713bcce58fac371048064963
Tin Muhar <tmuhar22@student.foi.hr>
2024-06-10 23:42:55 +0200

    Kreiran StudentRepository

--- Software/Zadaca_03/Zadaca_03/Models/Student.cs
+++ Software/Zadaca_03/Zadaca_03/Models/Student.cs
@@ -8,9 +8,8 @@ namespace Zadaca_03.Models
 {
     public class Student : Person
     {
-        public int OrderNumber { get; set; }
-        public int Review { get; set; }
-        public string Komentar { get; set; }
+        public int NumberOfReviews { get; set; }
+        
 
     }
 }
